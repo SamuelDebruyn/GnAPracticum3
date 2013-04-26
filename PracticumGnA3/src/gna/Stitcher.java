2b33542cbee327e1ad6532161cf2590811ef3714
SamuelDebruyn <github.s@muel.be>
2013-04-26 17:07:41 +0200

    optimalisatie: snelheid x 60

--- PracticumGnA3/src/gna/Stitcher.java
+++ PracticumGnA3/src/gna/Stitcher.java
@@ -35,54 +35,61 @@ public class Stitcher {
 
 		int height = image1.length;
 		int width = image1[0].length;
-		
+
 		// end node
 		Position target = new Position(width - 1, height - 1);
 
 		PriorityQueue<State> queue = new PriorityQueue<State>(11, new StateComparator());
+		HashMap<Position, State> inQueue = new HashMap<Position, State>();
 		HashSet<Position> closed = new HashSet<Position>();
 
 		// start node
-		queue.add(new State(new Position(0, 0), image1[0][0], image2[0][0]));
+		Position zero = new Position(0, 0);
+		State startNode = new State(zero, image1[0][0], image2[0][0]);
+		queue.add(startNode);
+		inQueue.put(zero, startNode);
 
-		while(!queue.peek().getPosition().equals(target)){
+
+		while (!queue.peek().getPosition().equals(target)) {
+
 			State currentState = queue.poll();
-			if(!closed.contains(currentState.getPosition()))
+			inQueue.remove(currentState.getPosition());
+
+			if (!closed.contains(currentState.getPosition())) {
 				// evaluated nodes
 				closed.add(currentState.getPosition());
-			for(Position pos : currentState.getPosition().getNeighbors(width, height, true)){
-				// neighboring node
-				State neighbor = new State(pos, image1[pos.getY()][pos.getX()], image2[pos.getY()][pos.getX()], currentState);
-				if(!closed.contains(neighbor.getPosition())){
-					boolean addN = false;
-					if(queue.contains(neighbor)){
+
+				for (Position pos : currentState.getPosition().getNeighbors(width, height, true)) {
+					// neighboring node
+					State neighbor = new State(pos,	image1[pos.getY()][pos.getX()],	image2[pos.getY()][pos.getX()], currentState);
+
+					if (closed.contains(neighbor.getPosition())) {
+						//NOP
+					}else if (inQueue.containsKey(neighbor.getPosition())) {
 						// neighbor is already in the queue
-						Iterator<State> itr = queue.iterator();
-						boolean found = false;
-						while(itr.hasNext() && !found){
-							State check = itr.next();
-							if(neighbor.equals(check)){
-								found = true;
-								if(neighbor.getTotalCost() < check.getTotalCost()){
-									// only replace the neighbor if the new path has a lower cost
-									itr.remove();
-									addN = true;
-								}
-							}
+
+						if (neighbor.getTotalCost() < inQueue.get(neighbor.getPosition()).getTotalCost()) {
+							// only replace the neighbor if the new path has a lower cost
+							queue.remove(inQueue.get(neighbor.getPosition()));
+							inQueue.put(neighbor.getPosition(), neighbor);
+							queue.add(neighbor);
 						}
-					}else{
-						addN = true;
+
+					} else {
+						inQueue.put(neighbor.getPosition(), neighbor);
+						queue.add(neighbor);
 					}
-					if(addN)
-						queue.offer(neighbor);
+					
 				}
+
 			}
 		}
+
 
 		ArrayList<Position> result = new ArrayList<Position>();
 
 		State current = queue.poll();
-		while(current != null){
+		while (current != null) {
 			result.add(current.getPosition());
 			current = current.getPrevious();
 		}
@@ -93,8 +100,9 @@ public class Stitcher {
 	}
 
 	/**
-	 * Apply the floodfill algorithm described in the assignment to mask. You can assume
-	 * the mask contains a seam from the upper left corner to the bottom right corner.
+	 * Apply the floodfill algorithm described in the assignment to mask. You
+	 * can assume the mask contains a seam from the upper left corner to the
+	 * bottom right corner.
 	 */
 	public void floodfill(int[][] mask) {
 
@@ -105,12 +113,12 @@ public class Stitcher {
 
 		HashMap<Integer, Integer> seamIndexes = new HashMap<Integer, Integer>();
 
-		for(int i = 0; i < height; i++){
-			for(int j = 0; j < width; j++){
-				if(mask[i][j] == SEAM)
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (mask[i][j] == SEAM)
 					seamIndexes.put(i, j);
 			}
-			if(seamIndexes.get(i) != width - 1){
+			if (seamIndexes.get(i) != width - 1) {
 				seamIndexes.put(-1, i);
 				break;
 			}
@@ -119,69 +127,71 @@ public class Stitcher {
 		int y = seamIndexes.get(-1);
 		int x = seamIndexes.get(y) + 1;
 		Position secondNode = new Position(x, y);
-		
+
 		// now floodfill the second part of the mask
 
 		LinkedList<Position> toFloodWithSecond = new LinkedList<Position>();
 		HashSet<Position> done = new HashSet<Position>();
 		toFloodWithSecond.offer(secondNode);
-		
-		while(!toFloodWithSecond.isEmpty()){
-			
+
+		while (!toFloodWithSecond.isEmpty()) {
+
 			Position current = toFloodWithSecond.poll();
-			
-			if(mask[current.getY()][current.getX()] != SEAM){
+
+			if (mask[current.getY()][current.getX()] != SEAM) {
 				mask[current.getY()][current.getX()] = IMAGE2;
-				
-				for(Position pos: current.getNeighbors(width, height, false)){
-					
-					if(!(done.contains(pos) || toFloodWithSecond.contains(pos)))
+
+				for (Position pos : current.getNeighbors(width, height, false)) {
+
+					if (!(done.contains(pos) || toFloodWithSecond.contains(pos)))
 						toFloodWithSecond.offer(pos);
-						
-				}	
-				
+
+				}
+
 			}
-			
+
 			done.add(current);
 
 		}
-		
+
 		/*
-		 * Code below only necessary if the values for IMAGE1, IMAGE2 and SEAM are changed.
-		 * This will not be the case (contacted TA). 
+		 * Code below only necessary if the values for IMAGE1, IMAGE2 and SEAM
+		 * are changed. This will not be the case (contacted TA).
 		 * 
-		LinkedList<Position> toFloodWithFirst = new LinkedList<Position>();
-		Position zero = new Position(0, 0);
-		toFloodWithFirst.addAll(zero.getNeighbors(width, height, false));
-		
-		while(!toFloodWithFirst.isEmpty()){
-			
-			Position current = toFloodWithFirst.poll();
-			
-			if(mask[current.getY()][current.getX()] != SEAM && mask[current.getY()][current.getX()] != IMAGE2){
-				mask[current.getY()][current.getX()] = IMAGE1;
-				
-				for(Position pos: current.getNeighbors(width, height, false)){
-					
-					if(!(done.contains(pos) || toFloodWithFirst.contains(pos)))
-						toFloodWithFirst.offer(pos);
-					
-				}
-				
-			}
-			
-			done.add(current);
-			
-		}*/
+		 * LinkedList<Position> toFloodWithFirst = new LinkedList<Position>();
+		 * Position zero = new Position(0, 0);
+		 * toFloodWithFirst.addAll(zero.getNeighbors(width, height, false));
+		 * 
+		 * while(!toFloodWithFirst.isEmpty()){
+		 * 
+		 * Position current = toFloodWithFirst.poll();
+		 * 
+		 * if(mask[current.getY()][current.getX()] != SEAM &&
+		 * mask[current.getY()][current.getX()] != IMAGE2){
+		 * mask[current.getY()][current.getX()] = IMAGE1;
+		 * 
+		 * for(Position pos: current.getNeighbors(width, height, false)){
+		 * 
+		 * if(!(done.contains(pos) || toFloodWithFirst.contains(pos)))
+		 * toFloodWithFirst.offer(pos);
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * done.add(current);
+		 * 
+		 * }
+		 */
 
 	}
 
 	/**
-	 * Return the mask to stitch two images together. The seam runs from the upper
-	 * left to the lower right corner, with the rightmost part coming from the
-	 * first image. A pixel in the mask is 0 on the places where <code>img1</code>
-	 * should be used, and 1 where <code>img2</code> should be used. On the seam
-	 * record a value of 2.
+	 * Return the mask to stitch two images together. The seam runs from the
+	 * upper left to the lower right corner, with the rightmost part coming from
+	 * the first image. A pixel in the mask is 0 on the places where
+	 * <code>img1</code> should be used, and 1 where <code>img2</code> should be
+	 * used. On the seam record a value of 2.
 	 * 
 	 * ImageCompositor will only call this method (not seam and floodfill) to
 	 * stitch two images.
@@ -194,12 +204,12 @@ public class Stitcher {
 		Iterable<Position> seam = seam(image1, image2);
 		int[][] mask = new int[image1.length][image1[0].length];
 
-		for(Position pos: seam){
+		for (Position pos : seam) {
 			mask[pos.getY()][pos.getX()] = SEAM;
 		}
 
 		floodfill(mask);
-		return mask;	  
+		return mask;
 	}
 
 }
